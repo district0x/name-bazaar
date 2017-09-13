947abbe1c630b5cd7359cb6ff80ce70787a322f8
madvas <matus.lestan@gmail.com>
2017-09-13 16:16:27 +0700

    added About & How it works page placeholder

--- src/name_bazaar/ui/pages/about_page.cljs
+++ src/name_bazaar/ui/pages/about_page.cljs
@@ -1,5 +1,13 @@
 (ns name-bazaar.ui.pages.about-page
-  (:require [district0x.ui.components.misc :as misc :refer [row row-with-cols col center-layout paper page]]))
+  (:require
+    [district0x.ui.components.misc :as misc :refer [row row-with-cols col paper page]]
+    [name-bazaar.ui.components.misc :refer [a side-nav-menu-center-layout]]
+    [name-bazaar.ui.styles :as styles]))
 
 (defmethod page :route/about []
-  [:div "About Page"])
+  [side-nav-menu-center-layout
+   [paper
+    [:h1
+     {:style styles/page-headline}
+     "About"]
+    [:div "Enter text here"]]])
--- src/name_bazaar/ui/pages/how_it_works_page.cljs
+++ src/name_bazaar/ui/pages/how_it_works_page.cljs
@@ -1,5 +1,13 @@
 (ns name-bazaar.ui.pages.how-it-works-page
-  (:require [district0x.ui.components.misc :as misc :refer [row row-with-cols col center-layout paper page]]))
+  (:require
+    [district0x.ui.components.misc :as misc :refer [row row-with-cols col paper page]]
+    [name-bazaar.ui.components.misc :refer [a side-nav-menu-center-layout]]
+    [name-bazaar.ui.styles :as styles]))
 
 (defmethod page :route/how-it-works []
-  [:div "How It works page"])
+  [side-nav-menu-center-layout
+   [paper
+    [:h1
+     {:style styles/page-headline}
+     "How it works"]
+    [:div "Enter text here"]]])
