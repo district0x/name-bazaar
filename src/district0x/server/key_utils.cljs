39f28582047c4a9bd403be56508ad718c352afd4
fbielejec <filip@district0x.io>
2017-09-19 12:06:21 +0200

    encryption/decryption utils and base64 utility functions

--- src/district0x/server/key_utils.cljs
+++ src/district0x/server/key_utils.cljs
@@ -1,7 +1,21 @@
 (ns district0x.server.key-utils
   (:require
-   [cljsjs.eccjs :as eccjs]))
+   [cljsjs.eccjs :as eccjs]
+   [goog.crypt.base64 :as base64]))
 
 (defn generate-keypair []
-  (-> (.generate js/ecc (.-ENC_DEC js/ecc) )
-      (js->clj :keywordize-keys true)))
+  (let [keypair (js->clj (.generate js/ecc (.-ENC_DEC js/ecc) 256))]
+    {:public-key (get keypair "enc")
+     :private-key (get keypair "dec")}))
+
+(defn encrypt [public-key content]
+  (.encrypt js/ecc public-key content))
+
+(defn decrypt [private-key content]
+  (.decrypt js/ecc private-key content))
+
+(defn encode-base64 [s]
+  (base64/encodeString s))
+
+(defn decode-base64 [s]
+  (base64/decodeString s))
--- test/browser/key_utils_tests.cljs
+++ test/browser/key_utils_tests.cljs
@@ -2,11 +2,20 @@
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
             [district0x.server.key-utils :as key-utils]))
 
+(defn contains-many? [m & ks]
+  (every? #(contains? m %) ks))
+
 (deftest key-utils-tests
   (testing "keypair generation test."
+    (is (contains-many? (key-utils/generate-keypair) :public-key :private-key)))
 
-   (prn key-utils/generate-keypair)    
-   
- (is (= 1 1))
-
-    ))
+  (testing "content decryption/encryption test."
+    (let [keypair (key-utils/generate-keypair)
+          content "top secret"
+          base64-encrypted-content (->> content
+                                        (key-utils/encrypt (:public-key keypair)) 
+                                        (key-utils/encode-base64))
+          decoded-content (->> base64-encrypted-content
+                               (key-utils/decode-base64)
+                               (key-utils/decrypt (:private-key keypair)))]
+      (is (= content decoded-content)))))
