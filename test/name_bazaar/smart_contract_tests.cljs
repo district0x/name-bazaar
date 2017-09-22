b98aebfbb495734a2656bc876992cac4361d4223
Mike Konkov <noospheratum@gmail.com>
2017-09-22 14:03:17 +0200

    Make offer requires ownership and ownership of the deed test

--- test/name_bazaar/smart_contract_tests.cljs
+++ test/name_bazaar/smart_contract_tests.cljs
@@ -81,50 +81,7 @@
                (is (tx-sent? (<! (registrar/register! ss
                                                       {:ens.record/label "abc"}
                                                       {:from (state/my-address 0)})))))
-             (testing "Registering name to transfer onwnership"
-               (is (tx-sent? (<! (registrar/register! ss
-                                                      {:ens.record/label "notowned"}
-                                                      {:from (state/my-address 0)}))))
-               (comment
-                 "This compiles and works")
-               (<! (deed/owner ss (:registrar.entry.deed/address
-                                   (second
-                                    (<! (registrar/entry ss
-                                                         {:ens.record/label "notowned"}
-                                                         {:from (state/my-address 0)}))))
-                               (state/my-address 1)))
 
-               (comment
-                 "This doesn't compile"
-                 (let [deed-addr
-                       (:registrar.entry.deed/address
-                        (second
-                         (<! (registrar/entry ss
-                                              {:ens.record/label "notowned"}
-                                              {:from (state/my-address 0)}))))]
-                   (is (not (empty? deed-addr)))
-                   (is (tx-sent? (<! (deed/owner ss deed-addr (state/my-address 1))))))))
-
-             (testing "Registering name to transfer deed"
-               (is (tx-sent? (<! (registrar/register! ss
-                                                      {:ens.record/label "notowndeed"}
-                                                      {:from (state/my-address 0)}))))
-               (is (tx-sent? (<! (registrar/transfer! ss
-                                                      {:ens.record/label "notowndeed"
-                                                       :ens.record/owner (state/my-address 0)}
-                                                      {:from (state/my-address 0)})))))
-             (testing
-                 "Can't make an offer on not owned domain"
-               (is (tx-failed? (<! (buy-now-offering-factory/create-offering! ss
-                                                                            {:offering/name "abc.eth"
-                                                                             :offering/price (eth->wei 0.1)}
-                                                                            {:from (state/my-address 1)})))))
-             #_(testing
-                 "Can make an offer on owned subdomain"
-               (is (tx-sent? (<! (buy-now-offering-factory/create-offering! ss
-                                                                            {:offering/name "sub.abc.eth"
-                                                                             :offering/price (eth->wei 0.1)}
-                                                                            {:from (state/my-address 0)})))))
              (testing
                  "Making an instant offer"
                (is (tx-sent? (<! (buy-now-offering-factory/create-offering! ss
@@ -280,3 +237,64 @@
              ;; TODO more
              ))))
 
+(deftest offering-name-ownership
+  (async done
+         (let [ss @*server-state*]
+           (go
+             (testing "Registering name to transfer onwnership"
+               (is (tx-sent? (<! (registrar/register! ss
+                                                      {:ens.record/label "notowned"}
+                                                      {:from (state/my-address 0)}))))
+
+               (let [deed-addr
+                     (:registrar.entry.deed/address
+                      (second
+                       (<! (registrar/entry ss
+                                            {:ens.record/label "notowned"}
+                                            {:from (state/my-address 0)}))))]
+                 (is (not (empty? deed-addr)))
+                 (is (not (empty? (second (<! (deed/owner ss deed-addr (state/my-address 1)))))))))
+
+             (testing "Registering name to transfer deed"
+               (is (tx-sent? (<! (registrar/register! ss
+                                                      {:ens.record/label "notowndeed"}
+                                                      {:from (state/my-address 0)}))))
+               (is (tx-sent? (<! (registrar/transfer! ss
+                                                      {:ens.record/label "notowndeed"
+                                                       :ens.record/owner (state/my-address 0)}
+                                                      {:from (state/my-address 0)})))))
+             (testing
+                 "Can't make an instant offer on not owned domain"
+               (is (tx-failed? (<! (buy-now-offering-factory/create-offering! ss
+                                                                            {:offering/name "notowned"
+                                                                             :offering/price (eth->wei 0.1)}
+                                                                            {:from (state/my-address 1)})))))
+             (testing
+                 "Can't make an instant offer on not owned domain"
+               (is (tx-failed? (<! (buy-now-offering-factory/create-offering! ss
+                                                                              {:offering/name "notowndeed"
+                                                                               :offering/price (eth->wei 0.1)}
+                                                                              {:from (state/my-address 1)})))))
+
+             (testing
+                 "Can't offer for bid name I don't manage"
+                 (is (tx-failed? (<! (auction-offering-factory/create-offering!
+                                    ss
+                                    {:offering/name "notowned"
+                                     :offering/price (eth->wei 0.1)
+                                     :auction-offering/end-time (to-epoch (time/plus (time/now) (time/weeks 2)))
+                                     :auction-offering/extension-duration 0
+                                     :auction-offering/min-bid-increase (web3/to-wei 0.1 :ether)}
+                                    {:from (state/my-address 0)})))))
+             (testing
+                   "Can't offer for bid name I don't own"
+               (is (tx-failed? (<! (auction-offering-factory/create-offering!
+                                    ss
+                                    {:offering/name "notowndeed"
+                                     :offering/price (eth->wei 0.1)
+                                     :auction-offering/end-time (to-epoch (time/plus (time/now) (time/weeks 2)))
+                                     :auction-offering/extension-duration 0
+                                     :auction-offering/min-bid-increase (web3/to-wei 0.1 :ether)}
+                                    {:from (state/my-address 0)})))))
+             ;; TODO more
+             (done)))))
