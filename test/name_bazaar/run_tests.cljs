4c494b178801303e0f7bcadfd000bef2d9966c8b
Mike Konkov <noospheratum@gmail.com>
2017-10-02 19:33:27 +0200

    silencing failed tx messages

--- test/name_bazaar/run_tests.cljs
+++ test/name_bazaar/run_tests.cljs
@@ -4,9 +4,22 @@
     [name-bazaar.smart-contract-tests]
     [name-bazaar.smart-contract-ext-tests]
     [name-bazaar.smart-contract-altering-tests]
+    [clojure.string :as str]
     [cljs.test :refer-macros [run-tests]]))
 
-(nodejs/enable-util-print!)
+(defn enable-util-print! []
+  (set! *print-newline* false)
+  (set! *print-fn*
+        (fn [& args]
+          (when-not (str/includes? (first args)
+                                   "RuntimeError Error: VM Exception while processing transaction:")
+            (.apply (.-log js/console) js/console (into-array args)))))
+  (set! *print-err-fn*
+        (fn [& args]
+          (.apply (.-error js/console) js/console (into-array args))))
+  nil)
+(enable-util-print!)
+
 (set! (.-error js/console) (fn [x] (.log js/console x)))
 
 (comment
