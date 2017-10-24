6a4531cad9a88f34a1747467ffd2060cd9b7ca27
Mike Konkov <noospheratum@gmail.com>
2017-10-24 16:23:17 +0200

    unused event

--- src/name_bazaar/ui/core.cljs
+++ src/name_bazaar/ui/core.cljs
@@ -51,7 +51,6 @@
                                      :events #{:district0x/set-active-page}
                                      :dispatch-to [:active-page-changed]}
                     :dispatch-n [[:setup-update-now-interval]
-                                 [:setup-address-reload-interval]
                                  [:district0x/load-conversion-rates [:USD]]
                                   [:district0x.config/load]]}}])
   (mount-root))
