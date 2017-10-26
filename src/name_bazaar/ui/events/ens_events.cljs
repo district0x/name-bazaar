c8aeb58b16ed6868e879723cfc47f10f386a3512
Mike Konkov <noospheratum@gmail.com>
2017-10-26 14:56:11 +0200

    Compiles resolver

--- src/name_bazaar/ui/events/ens_events.cljs
+++ src/name_bazaar/ui/events/ens_events.cljs
@@ -12,7 +12,9 @@
     [name-bazaar.ui.utils :refer [namehash sha3 parse-query-params path-for get-ens-record-name get-offering-name get-offering]]
     [re-frame.core :as re-frame :refer [reg-event-fx inject-cofx path after dispatch trim-v console]]
     [district0x.shared.utils :as d0x-shared-utils]
-    [medley.core :as medley]))
+    [medley.core :as medley]
+    [taoensso.timbre :as logging :refer-macros [info warn error]]
+    ))
 
 (reg-event-fx
   :ens/set-owner
@@ -35,6 +37,7 @@
   :ens.records/load
   interceptors
   (fn [{:keys [:db]} [nodes {:keys [:load-resolver?]}]]
+    (info [:LOADING-NODES nodes])
     (let [instance (get-instance db :ens)]
       {:web3-fx.contract/constant-fns
        {:fns (concat
@@ -68,6 +71,7 @@
   :ens.records.owner/loaded
   interceptors
   (fn [{:keys [:db]} [node owner]]
+    (info ["Loaded owner" node owner])
     {:db (assoc-in db [:ens/records node :ens.record/owner] (if (= owner "0x")
                                                               d0x-shared-utils/zero-address
                                                               owner))}))
@@ -76,6 +80,7 @@
   :ens.records.resolver/loaded
   interceptors
   (fn [{:keys [:db]} [node resolver]]
+    (info ["Loaded resolver" node resolver])
     {:db (assoc-in db [:ens/records node :ens.record/resolver] (if (= resolver "0x")
                                                                  d0x-shared-utils/zero-address
                                                                  resolver))}))
