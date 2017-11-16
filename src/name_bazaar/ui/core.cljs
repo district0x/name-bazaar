c3b55c3f1008337c1dc6a8685aa9a3102ea574b4
madvas <matus.lestan@gmail.com>
2017-11-16 21:42:04 +0700

    putting back my-addresses-loaded

--- src/name_bazaar/ui/core.cljs
+++ src/name_bazaar/ui/core.cljs
@@ -46,7 +46,7 @@
                    :effects
                    {:async-flow {:first-dispatch [:district0x/load-smart-contracts {:version constants/contracts-version}]
                                  :rules [{:when :seen?
-                                          :events [:district0x/smart-contracts-loaded]
+                                          :events [:district0x/smart-contracts-loaded :district0x/my-addresses-loaded]
                                           :dispatch-n [[:district0x/watch-my-eth-balances]
                                                        [:try-resolving-address]
                                                        [:active-page-changed]]}]}
