b391314226ffe4986a90b2f90423fac8bb46a49d
fbielejec <filip@district0x.io>
2017-11-16 18:01:32 +0100

    matching meta tags

--- src/name_bazaar/ui/components/meta_tags.cljs
+++ src/name_bazaar/ui/components/meta_tags.cljs
@@ -7,5 +7,5 @@
                                    description "A peer-to-peer marketplace for the exchange of names registered via the Ethereum Name Service."}}]
   (let [{:keys [:handler :query-params :path] :as active-page} @(subscribe [:district0x/active-page])]
     [d0x-meta-tags/meta-tags
-     [:title {:id "title" :title title}]
-     [:meta {:id "description" :content description}]]))
+     [:title {:id "title"} title]
+     [:meta {:id "description" :name "description" :content description}]]))
