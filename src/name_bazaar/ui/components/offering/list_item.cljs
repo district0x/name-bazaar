ac01afd51d53bb8cbaa8e9871c9b8fe9ae0e6877
Mike Konkov <noospheratum@gmail.com>
2018-02-06 23:50:24 +0100

    cleanup

--- src/name_bazaar/ui/components/offering/list_item.cljs
+++ src/name_bazaar/ui/components/offering/list_item.cljs
@@ -213,49 +213,6 @@
                [offering-header-price
                 {:offering offering}]]])]))))
 
-#_[ui/GridRow
-   {:ref #(reset! visible? true)                   ;; For fade-in animation
-    :class (str "search-results-list-item-header "
-                (when @visible? "opacity-1"))
-    :vertical-align :middle}
-   [ui/GridColumn
-    {:width 4}
-    [offering-header-main-text
-     {:show-created-on? show-created-on?
-      :offering offering}]]
-   [ui/GridColumn
-    {:width 2
-     :text-align :center}
-    [offering-header-offering-type
-     {:offering offering}]]
-   [ui/GridColumn
-    {:width 3
-     :text-align :center}
-    [:div.offering-auction-item
-     {:class (if (and auction? bid-count) "opacity-1" "opacity-0")}
-     [:div.amount bid-count]]]
-   [ui/GridColumn
-    {:width 3
-     :text-align :center}
-    (let [[unit amount] (if-not show-finalized-on?
-                          (time-remaining-biggest-unit (t/now) end-time)
-                          (time-remaining-biggest-unit finalized-on (t/now)))]
-      [:div.offering-auction-item.time-left
-       {:class (if (or (and auction? end-time)
-                       show-finalized-on?)
-                 :opacity-1
-                 :opacity-0)}
-       [:div.amount amount " " [:span.unit (cond-> (time-unit->short-text unit)
-                                             (contains? #{:days :hours} unit) (pluralize amount))]]])]
-   [ui/GridColumn
-    {:class "price-column"
-     :width 4
-     :text-align :right
-     :vertical-align :middle}
-    [offering-header-tags props]
-    [offering-header-price
-     {:offering offering}]]]
-
 (defn offering-list-item []
   (let [mobile? (subscribe [:district0x.window.size/mobile?])]
     (fn [{:keys [:offering :expanded? :on-expand :key :header-props :body-props :disable-expand? :on-click]
