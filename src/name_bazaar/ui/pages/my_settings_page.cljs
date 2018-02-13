03a7b984a3e3edd6f3baca8e95d6b35e2bbace89
Mike Konkov <noospheratum@gmail.com>
2018-02-13 17:56:58 +0100

    comment cleanup

--- src/name_bazaar/ui/pages/my_settings_page.cljs
+++ src/name_bazaar/ui/pages/my_settings_page.cljs
@@ -42,43 +42,7 @@
                          (dispatch [:district0x-emails/set-email
                                     {:district0x-emails/email @email-value}
                                     {:on-tx-receipt [:district0x.snackbar/show-message "Your email was successfully updated"]}]))}
-            "Save"]]]
-         #_[ui/Grid
-          {:class "layout-grid submit-footer"}
-          [ui/GridRow
-           [ui/GridColumn
-            {:mobile 16
-             :tablet 8
-             :computer 6}
-            [input
-             {:label "Email"
-              :fluid true
-              :value @email-value
-              :error (not valid?)
-              :on-change #(reset! email-value (aget %2 "value"))}]]]
-          [ui/GridRow
-           [ui/GridColumn
-            {:class :join-upper}
-            (when @active-address-has-email?
-              [:div.description.success "Your address has encrypted email associated already"])
-            [:div.description
-             "Email associated with your address will be encrypted and stored on a public blockchain. "
-             "Only our email server will be able to decrypt it. We'll use it to send you notifications about
-             your purchases, sells and offering requests."]]]
-          [ui/GridRow
-           {:centered true}
-           [:div
-            [transaction-button
-             {:primary true
-              :disabled (or (empty? @email-value)
-                            (not valid?))
-              :pending? @(subscribe [:district0x-emails.set-email/tx-pending? @active-address])
-              :pending-text "Saving..."
-              :on-click (fn []
-                          (dispatch [:district0x-emails/set-email
-                                     {:district0x-emails/email @email-value}
-                                     {:on-tx-receipt [:district0x.snackbar/show-message "Your email was successfully updated"]}]))}
-             "Save"]]]]]))))
+            "Save"]]]]))))
 
 (defmethod page :route.user/my-settings []
   [app-layout {:meta {:title "NameBazaar - My Settings" :description "Configure settings associated with your Ethereum address."}}
