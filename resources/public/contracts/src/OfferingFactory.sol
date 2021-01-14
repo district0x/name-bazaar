e274b677c938b79b892894bf53052b4df980d442
Emanuel Tesar <e.tesarr@gmail.com>
2021-01-14 14:16:16 +0100

    Add strings library as submodule

--- resources/public/contracts/src/OfferingFactory.sol
+++ resources/public/contracts/src/OfferingFactory.sol
@@ -4,7 +4,7 @@ import "ens-repo/contracts/ENS.sol";
 import "ens-repo/contracts/HashRegistrarSimplified.sol";
 import "OfferingRegistry.sol";
 import "OfferingRequestsAbstract.sol";
-import "strings.sol";
+import "strings-lib-repo/strings.sol";
 
 /**
  * @title OfferingFactory
