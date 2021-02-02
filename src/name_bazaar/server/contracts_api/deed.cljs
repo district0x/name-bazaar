ff08e11518a11b19feb61422a5e73ef6abf07b47
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-02-02 15:18:15 +0100

    Update contracts API

--- src/name_bazaar/server/contracts_api/deed.cljs
+++
@@ -1,7 +0,0 @@
-(ns name-bazaar.server.contracts-api.deed
-  (:require
-    [district.server.smart-contracts :refer [contract-call]]))
-
-(defn owner [contract-address]
-  (contract-call [:deed contract-address] :owner))
-
