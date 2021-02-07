3b481ecfb90629d59775c977f4af28bf0a557958
Emanuel Tesar <e.tesarr@gmail.com>
2021-02-07 11:56:11 +0100

    Simplify calling generate data function

--- dev/cljs/user.cljs
+++ dev/cljs/user.cljs
@@ -5,15 +5,18 @@
 
 (log/info "Initialized Development Environment!")
 (log/info "Run (redeploy) in the CLJS REPL for a fresh environment.")
+(log/info "Run (generate-data) to initialize sample dev data")
 
 (defn redeploy
   "Redeploy Smart Contracts"
   []
-  (log/info "Redeployment is a long process, please be patient...")
-  (.nextTick
-   js/process
-   (fn []
-     (name-bazaar.server.dev/redeploy)
-     (log/info "Redeployment Finished!"))))
+  (name-bazaar.server.dev/redeploy)
+  (log/info "Redeployment Finished!"))
+
+(defn generate-data
+  "Generate sample dev data"
+  []
+  (name-bazaar.server.dev/generate-data)
+  (log/info "Data generation Finished!"))
 
 (set! *main-cli-fn* name-bazaar.server.dev/-main)
--- src/name_bazaar/server/dev.cljs
+++ src/name_bazaar/server/dev.cljs
@@ -1,37 +1,18 @@
 (ns name-bazaar.server.dev
   (:require
-    [cljs-http.client :as http]
     [cljs-time.coerce :refer [to-epoch]]
-    [cljs-time.core :as t]
-    [cljs-web3.core :as web3]
-    [cljs-web3.eth :as web3-eth]
     [cljs.nodejs :as nodejs]
     [cljs.pprint :as pprint]
-    [cljs.spec.alpha :as s]
     [district.server.config :refer [config]]
     [district.server.db :refer [db]]
-    [district.server.endpoints :as endpoints]
     [district.server.endpoints.middleware.logging :refer [logging-middlewares]]
     [district.server.logging]
     [district.server.smart-contracts]
     [district.server.web3 :refer [web3]]
     [district.server.web3-watcher]
     [goog.date.Date]
-    [honeysql.core :as sql]
-    [honeysql.format :as sql-format]
-    [honeysql.helpers :as sql-helpers]
     [mount.core :as mount]
     [name-bazaar.server.api]
-    [name-bazaar.server.contracts-api.auction-offering :as auction-offering]
-    [name-bazaar.server.contracts-api.auction-offering-factory :as auction-offering-factory]
-    [name-bazaar.server.contracts-api.buy-now-offering :as buy-now-offering]
-    [name-bazaar.server.contracts-api.buy-now-offering-factory :as buy-now-offering-factory]
-    [name-bazaar.server.contracts-api.ens :as ens]
-    [name-bazaar.server.contracts-api.offering :as offering]
-    [name-bazaar.server.contracts-api.offering-registry :as offering-registry]
-    [name-bazaar.server.contracts-api.offering-requests :as offering-requests]
-    [name-bazaar.server.contracts-api.registrar :as registrar]
-    [name-bazaar.server.contracts-api.used-by-factories :as used-by-factories]
     [name-bazaar.server.db]
     [name-bazaar.server.deployer]
     [name-bazaar.server.emailer]
@@ -45,24 +26,6 @@
 
 (def namehash (aget (nodejs/require "eth-ens-namehash") "hash"))
 (def sha3 (comp (partial str "0x") (aget (nodejs/require "js-sha3") "keccak_256")))
-
-(defn on-jsload []
-  (mount/stop #'district.server.endpoints/endpoints)
-  (mount/start #'district.server.endpoints/endpoints))
-
-(defn deploy-to-mainnet []
-  (mount/stop #'district.server.web3/web3
-              #'district.server.smart-contracts/smart-contracts)
-  (mount/start-with-args (merge
-                           (mount/args)
-                           {:web3 {:port 8545}
-                            :deployer {:write? true
-                                       :from "0x2a2A57a98a07D3CA5a46A0e1d51dEFffBeF54E4F"
-                                       :emergency-multisig "0x52f3f521c5f573686a78912995e9dedc5aae9928"
-                                       :skip-ens-registrar? true
-                                       :gas-price (web3/to-wei 4 :gwei)}})
-                         #'district.server.web3/web3
-                         #'district.server.smart-contracts/smart-contracts))
 
 (defn redeploy []
   (mount/stop)
