a94b92014d8e331d5a0986de4ac6510d409ac24f
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-02-11 18:58:11 +0100

    Transfer ownership fix

--- run-ganache.sh
+++ run-ganache.sh
@@ -3,6 +3,6 @@
 docker stop namebazaar-ganache || true
 docker rm namebazaar-ganache || true
 
-docker run --name=namebazaar-ganache -d \
+docker run --name=namebazaar-ganache \
     -p 8549:8549 \
     trufflesuite/ganache-cli:v6.12.1 -p 8549
