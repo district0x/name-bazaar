7020900de8bcbe12ff9ac03759c7df44b24a5822
Emanuel Tesar <e.tesarr@gmail.com>
2021-02-23 11:25:03 +0100

    Update slither script and mention in it README

--- run-slither.sh
+++ run-slither.sh
@@ -6,16 +6,18 @@ cd resources/public/contracts/src
 ln -s ../../../../node_modules/openzeppelin-solidity "openzeppelin-solidity"
 ln -s ../../../../node_modules/@ensdomains @ensdomains
 
-# Execute slither (which should be installed) and exclude findings found in
-# dependencies.
+# Executes slither and excludes findings found in dependencies.
 #
-# Exclude output of some slither detectors using the following arguments
+# To exclude output of some slither detectors using the following arguments
 # --exclude-low
 # --exclude-medium
 # --exclude-high
 # --exclude-informational
 # --exclude-optimization
-slither --exclude-low --exclude-medium --exclude-informational --exclude-high . 2>&1 | grep -E -v "openzeppelin|@ensdomains" | grep -E -B 1 "\.sol#"
+#
+# Many findings are false positives or only related to code style. Manual review
+# is still necessary.
+slither . --exclude-low --exclude-informational --exclude incorrect-equality 2>&1 | grep -E -v "openzeppelin|@ensdomains" | grep -E -B 1 "\.sol#"
 
 rm openzeppelin-solidity
 rm @ensdomains
