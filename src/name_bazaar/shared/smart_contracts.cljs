0f6df3e328aef1e25f7858a13869576bacb04be5
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-04-07 17:02:55 +0200

    Refactor fix

--- src/name_bazaar/shared/smart_contracts.cljs
+++ src/name_bazaar/shared/smart_contracts.cljs
@@ -2,26 +2,35 @@
 
 (def smart-contracts
   {:auction-offering-factory {:name "AuctionOfferingFactory",
-                              :address "0xc806e3343324d1925AD1633b745DD25dbB2BeC75"},
+                              :address "0x959113e1c88d7e866e168ded02bf5cf8aaba2e0f"},
    :buy-now-offering-factory {:name "BuyNowOfferingFactory",
-                              :address "0x23d4a24Fa4981B9b3Ea2F97aA7D87C6055ee08AA"},
-   :name-bazaar-registrar {:name "NameBazaarDevRegistrar",
-                           :address "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"},
+                              :address "0xcaf0523f1ca2985b5cbba8a42a8cc413ff7512d0"},
+   ;; Ropsten BaseRegistrarImplementation
+   ;; https://ropsten.etherscan.io/address/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85
+   :eth-registrar {:name "NameBazaarDevRegistrar",
+                   :address "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"},
    :buy-now-offering {:name "BuyNowOffering",
-                      :address "0x70c71A773Bf0Dd5242bF15f7161fc47Ec15822d8"},
+                      :address "0x57001d4d427b9c862544b56b84a5520db10206b0"},
+   ;; Ropsten ReverseRegistrar
+   ;; https://ropsten.etherscan.io/address/0x6F628b68b30Dc3c17f345c9dbBb1E483c2b7aE5c
    :reverse-registrar {:name "NamebazaarDevReverseRegistrar",
                        :address "0x6F628b68b30Dc3c17f345c9dbBb1E483c2b7aE5c"},
+   ;; Ropsten PublicResolver
+   ;; https://ropsten.etherscan.io/address/0x42d63ae25990889e35f215bc95884039ba354115
    :public-resolver {:name "NamebazaarDevPublicResolver",
                      :address "0x42D63ae25990889E35F215bC95884039Ba354115"},
    :reverse-name-resolver {:name "NamebazaarDevNameResolver",
                            :address "0x0000000000000000000000000000000000000000"},
+   ;; Ropsten ENSRegistry
+   ;; https://docs.ens.domains/ens-deployments
+   ;; https://ropsten.etherscan.io/address/0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e
    :ens {:name "ENSRegistry",
          :address "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"},
    :offering-registry {:name "OfferingRegistry",
-                       :address "0x3A67Fd570F823E4c4218D8C1143ffcd088E3bfA7"},
+                       :address "0x501fa189c6ed1789cf6be5728c5e4b0a94d0c4b2"},
    :district0x-emails {:name "District0xEmails",
-                       :address "0x850c009B2b4328Ef5c5Eac56c7F05D5184d1114D"},
+                       :address "0x1383405e11d179581ae3128c0c1698be29e40565"},
    :offering-requests {:name "OfferingRequests",
-                       :address "0x84ada9009575Fdc5E06896a2d815574d6F625AEa"},
+                       :address "0x80afaa1b9a5717731d0d874c37e2b535dc1502a9"},
    :auction-offering {:name "AuctionOffering",
-                      :address "0x4a15888DBA04Ff39A21D3BC137D88b493eC273E9"}})
+                      :address "0x0fc5ae5770a8188e1dcf46777020c186884d44dc"}})
