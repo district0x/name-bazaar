8d7646b1758408aa0faa037f2078a979640df3ec
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-05-12 12:49:02 +0200

    Fix emailer
    Generate a working public-private keypair from eccjs library
    for use in dev builds. Use web3-events and fix format
    discrepancies.

--- src/name_bazaar/server/emailer.cljs
+++ src/name_bazaar/server/emailer.cljs
@@ -1,14 +1,17 @@
 (ns name-bazaar.server.emailer
   (:require
-    [cljs-web3.eth :as web3-eth]
+    [cljs.core.async :refer [<!]]
     [district.encryption :as encryption]
     [district.sendgrid :refer [send-email]]
     [district.server.config :refer [config]]
+    [district.server.web3 :refer [web3]]
+    [district.server.web3-events :as web3-events]
+    [district.shared.async-helpers :refer [safe-go]]
+    [district0x.shared.utils :refer [hex-to-utf8]]
     [goog.format.EmailAddress :as email-address]
     [mount.core :as mount :refer [defstate]]
     [name-bazaar.server.contracts-api.district0x-emails :refer [get-email]]
     [name-bazaar.server.contracts-api.offering :as offering]
-    [name-bazaar.server.contracts-api.offering-registry :as offering-registry]
     [name-bazaar.server.contracts-api.offering-requests :refer [get-request get-requesters]]
     [name-bazaar.server.emailer.templates :as templates]
     [name-bazaar.server.generator]
@@ -37,15 +40,15 @@
 
 (defn on-offering-added [{:keys [:offering :node :owner :version] :as args}]
   (info info-text {:args args} ::on-offering-added)
-  (try
-    (let [{:keys [:offering-request/name :offering-request/latest-round]} (get-request {:offering-request/node node})
+  (safe-go
+    (let [{:keys [:offering-request/name :offering-request/latest-round]} (<! (get-request {:offering-request/node node}))
           round (dec latest-round)
-          requesters (get-requesters {:offering-request/node node
-                                      :offering-request/round round})]
+          requesters (<! (get-requesters {:offering-request/node node
+                                          :offering-request/round round}))]
       (if (empty? requesters)
         (info "No requesters found for offering" {:offering offering :name name :node node :round round} ::on-offering-added)
         (doseq [requester requesters]
-          (let [base64-encrypted-email (get-email {:district0x-emails/address requester})]
+          (let [base64-encrypted-email (<! (get-email {:district0x-emails/address requester}))]
             (when-let [to (validate-email base64-encrypted-email)]
               (send-email {:from from
                            :to to
@@ -63,17 +66,18 @@
 
 (defn- on-auction-finalized [offering original-owner winning-bidder name price]
   (info info-text {:offering offering} ::on-auction-finalized)
-  (try
-    (let [owner-encrypted-email (get-email {:district0x-emails/address original-owner})
-          winner-encrypted-email (get-email {:district0x-emails/address winning-bidder})]
+  (safe-go
+    (let [owner-encrypted-email (<! (get-email {:district0x-emails/address original-owner}))
+          winner-encrypted-email (<! (get-email {:district0x-emails/address winning-bidder}))
+          offering-address (:offering/address offering)]
       (if-let [to (validate-email owner-encrypted-email)]
         (send-email {:from from
                      :to to
                      :subject (str "Your auction was finalized: " name)
-                     :content (templates/on-auction-finalized :owner offering name price)
+                     :content (templates/on-auction-finalized :owner offering-address name price)
                      :substitutions {:header (str name " auction has ended")
                                      :button-title "See auction details"
-                                     :button-href (templates/form-link offering)}
+                                     :button-href (templates/form-link offering-address)}
                      :on-success #(info "Success sending email to owner" {:address original-owner} ::on-auction-finalized)
                      :on-error #(error "Error sending email to owner" {:error %} ::on-auction-finalized)
                      :template-id template-id
@@ -85,10 +89,10 @@
         (send-email {:from from
                      :to to
                      :subject (str "Auction was finalized: " name)
-                     :content (templates/on-auction-finalized :winner offering name price)
+                     :content (templates/on-auction-finalized :winner offering-address name price)
                      :substitutions {:header (str name " auction has ended")
                                      :button-title "See auction details"
-                                     :button-href (templates/form-link offering)}
+                                     :button-href (templates/form-link offering-address)}
                      :on-success #(info "Success sending email to winner" {:address winning-bidder})
                      :on-error #(error "Error sending email to winner" {:error %})
                      :template-id template-id
@@ -99,43 +103,31 @@
 
 (defn- on-offering-bought [offering original-owner name price]
   (info info-text {:offering offering} ::on-offering-bought)
-  (try
-    (let [owner-encrypted-email (get-email {:district0x-emails/address original-owner})]
+  (safe-go
+    (let [owner-encrypted-email (<! (get-email {:district0x-emails/address original-owner}))
+          offering-address (:offering/address offering)]
       (when-let [to (validate-email owner-encrypted-email)]
         (send-email {:from from
                      :to to
                      :subject (str "Your offering was bought: " name)
-                     :content (templates/on-offering-bought offering name price)
+                     :content (templates/on-offering-bought offering-address name price)
                      :on-success #(info "Success sending email to owner" {:address original-owner} ::on-offering-bought)
                      :on-error #(error "Error sending email to owner" {:error %} ::on-offering-bought)
                      :substitutions {:header (str name " was bought")
                                      :button-title "See offering details"
-                                     :button-href (templates/form-link offering)}
+                                     :button-href (templates/form-link offering-address)}
                      :template-id template-id
                      :api-key (:api-key @emailer)
                      :print-mode? (:print-mode? @emailer)})))))
 
 
-(defn on-offering-changed [{:keys [:offering :version :event-type :extra-data] :as args}]
-  (info info-text {:args args} ::on-offering-changed)
-  (try
-    (let [{:keys [:offering/name
-                  :offering/original-owner
-                  :offering/price
-                  :offering/winning-bidder]} (offering/get-offering offering)]
-      (if winning-bidder
-        (on-auction-finalized offering original-owner winning-bidder name price)
-        (on-offering-bought offering original-owner name price)))))
-
-
 (defn on-new-bid [{:keys [:offering] :as args}]
   (info info-text {:args args} ::on-new-bid)
-  (try
+  (safe-go
     (let [{:keys [:offering/name
                   :offering/original-owner
-                  :offering/price]} (offering/get-offering offering)
-
-          owner-encrypted-email (get-email {:district0x-emails/address original-owner})]
+                  :offering/price]} (<! (offering/get-offering offering))
+          owner-encrypted-email (<! (get-email {:district0x-emails/address original-owner}))]
       (when-let [to (validate-email owner-encrypted-email)]
         (send-email {:from from
                      :to to
@@ -151,23 +143,39 @@
                      :print-mode? (:print-mode? @emailer)})))))
 
 
-(defn event-callback [f]
-  (fn [err res]
-    (if err
-      (error "Emailer got error from blockchain event" {:error err} ::event-callback)
-      (f (:args res)))))
+(defn on-offering-changed [{:keys [:offering :version :event-type :extra-data] :as args}]
+  (info info-text {:args args} ::on-offering-changed)
+  (case (hex-to-utf8 @web3 event-type)
+    "bid" (on-new-bid args)
+    "finalize" (safe-go
+                 (let [offering (<! (offering/get-offering offering))
+                       {:keys [:offering/name
+                               :offering/original-owner
+                               :offering/price
+                               :offering/winning-bidder]} offering
+                       ]
+                   (if winning-bidder
+                     (on-auction-finalized offering original-owner winning-bidder name price)
+                     (on-offering-bought offering original-owner name price))))
+    "uninteresting event"))
+
+
+(defn- dispatcher [callback]
+  (fn [err {:keys [:latest-event? :args] :as ev}]
+    (when latest-event?
+      (if err
+        (error "Emailer got error from blockchain event" {:error err} ::event-callback)
+        (callback args)))))
 
 
 (defn start [{:keys [:api-key :private-key :print-mode?] :as opts}]
   (when-not private-key
     (throw (js/Error. ":private-key is required to start emailer")))
-  (merge opts
-         {:listeners
-          [(offering-registry/on-offering-added {} "latest" (event-callback on-offering-added))
-           (offering-registry/on-offering-changed {:event-type "finalize"} "latest" (event-callback on-offering-changed))
-           (offering-registry/on-offering-changed {:event-type "bid"} "latest" (event-callback on-new-bid))]}))
+  (let [callback-ids
+        [(web3-events/register-callback! :offering-registry/offering-added (dispatcher on-offering-added))
+         (web3-events/register-callback! :offering-registry/offering-changed (dispatcher on-offering-changed))]]
+    (assoc opts :callback-ids callback-ids)))
 
 
 (defn stop [emailer]
-  (doseq [listener (remove nil? (:listeners @emailer))]
-    (web3-eth/stop-watching! listener (fn [err]))))
+  (web3-events/unregister-callbacks! (:callback-ids @emailer)))
