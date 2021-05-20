fc0ff2b24623fa0394e61a024628121ee98ab484
sm47916 <sergii@district0x.io>
2021-05-20 16:09:31 +0200

    update dockerfiles, add qa config, update nginx

--- src/name_bazaar/ui/config.cljs
+++ src/name_bazaar/ui/config.cljs
@@ -4,7 +4,7 @@
 ;; you need to rebuild UI for each new configuration
 ;; (e.g. change this file then build new docker UI image)
 ;; we should make UI configurable on the fly
-(goog-define environment "prod")
+(goog-define environment "qa")
 
 (def development-config
   {:debug? true
@@ -17,6 +17,17 @@
    :root-url "http://0.0.0.0:4544"
    :server-url "http://localhost:6200"})
 
+(def qa-config
+  {:debug? true
+   :logging {:level :warn
+             :console? true}
+   :pushroute-hosts "namebazaar.qa.district0x.io"
+   :infura-id "874e0519ba33487f89ef854b0179906c" ;; this is for wallet-connect only, which will not work when running with ganache
+   :fallback-web3-url "https://ropsten.infura.io/"
+   :load-node-addresses? true
+   :root-url "https://namebazaar.qa.district0x.io"
+   :server-url "qa_namebazaar-server:3000"})
+
 (def production-config
   {:logging {:level :warn
              :sentry {:dsn "https://597ef71a10a240b0949c3b482fe4b9a4@sentry.io/1364232"}}
@@ -25,9 +36,10 @@
    :fallback-web3-url "https://mainnet.infura.io/"
    :load-node-addresses? true
    :root-url "https://namebazaar.io"
-   :server-url "https://api.namebazaar.io"})
+   :server-url "prod_namebazaar-server:3000"})
 
 (def config
   (condp = environment
     "prod" production-config
+    "qa"  qa-config
     "dev" development-config))
