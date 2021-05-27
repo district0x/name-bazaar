bf58ea06eb9e63d7160ff3a3b709bbb37449b74d
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-05-27 14:24:03 +0200

    Fix name display in watched names

--- src/name_bazaar/ui/subs/watched_names_subs.cljs
+++ src/name_bazaar/ui/subs/watched_names_subs.cljs
@@ -12,12 +12,15 @@
   :watched-names/watched-items
   :<- [:watched-names]
   :<- [:ens/records]
-  (fn [[watched-names ens-records]]
+  :<- [:offerings]
+  (fn [[watched-names ens-records offerings]]
     (map (fn [node]
            (let [{:keys [:ens.record/name] :as watched-ens-record} (get-in watched-names [:ens/records node])]
              (merge
                watched-ens-record
-               {:ens.record/node node})))
+               {:ens.record/node node}
+               (if-let [offering-address (get-in ens-records [node :ens.record/active-offering])]
+                 (get offerings offering-address)))))
          (:order watched-names))))
 
 (reg-sub
