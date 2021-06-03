13564d785c7a08c29ed18a89619459f4a9e49afb
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-06-03 14:06:02 +0200

    TLD auction offerings in single transaction

--- resources/public/contracts/src/AuctionOfferingFactory.sol
+++ resources/public/contracts/src/AuctionOfferingFactory.sol
@@ -22,14 +22,14 @@ contract AuctionOfferingFactory is OfferingFactory {
     }
 
     /**
-    * @dev Deploys new Auction offering and registers it to OfferingRegistry
+    * @dev Deploys new auction offering and registers it to OfferingRegistry
     * @param name string Plaintext ENS name
     * @param startPrice uint The start price of the auction
     * @param endTime uint The end time of the auction
     * @param extensionDuration uint The extension duration of the auction
     * @param minBidIncrease uint The min bid increase of the auction
     */
-    function createOffering(
+    function createSubnameOffering(
         // WARNING: The contract DOES NOT perform ENS name normalisation, which is up to responsibility of each offchain UI!
         string calldata name,
         uint startPrice,
@@ -57,8 +57,63 @@ contract AuctionOfferingFactory is OfferingFactory {
         registerSubnameOffering(node, labelHash, forwarder, version);
     }
 
-    function onERC721Received(address from, address to, uint256 tokenId, bytes memory data) public returns (bytes4) {
+    /**
+    * @dev Deploys new TLD auction offering and registers it to OfferingRegistry
+    * @param name string Plaintext ENS name
+    * @param startPrice uint The start price of the auction
+    * @param endTime uint The end time of the auction
+    * @param extensionDuration uint The extension duration of the auction
+    * @param minBidIncrease uint The min bid increase of the auction
+    * @param originalOwner address Original owner of the name
+    */
+    function createTLDOffering(
+        // WARNING: The contract DOES NOT perform ENS name normalisation, which is up to responsibility of each offchain UI!
+        string memory name,
+        uint startPrice,
+        uint64 endTime,
+        uint64 extensionDuration,
+        uint minBidIncrease,
+        address payable originalOwner
+    ) internal {
+        address payable forwarder = address(new Forwarder());
+        bytes32 node = namehash(name);
+        bytes32 labelHash = getLabelHash(name);
+        uint128 version = 100001;                   // versioning for Auction offerings starts at number 100000
 
+        AuctionOffering(forwarder).construct(
+            node,
+            name,
+            labelHash,
+            originalOwner,
+            version,
+            startPrice,
+            endTime,
+            extensionDuration,
+            minBidIncrease
+        );
+
+        registerTLDOffering(node, labelHash, forwarder, version, originalOwner);
+    }
+
+    /**
+    * @dev Recognizes being sent .eth subdomain ownership via EthRegistrar and initiates offering creation
+    * @param operator address Who initiated the transfer
+    * @param from address Original owner of the domain
+    * @param tokenId uint256 EthRegistrar identifier of the domain
+    * @param data bytes memory Additional data sent with transfer encoding the desired offering parameters
+    */
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data)
+        public
+        onlyRegistrar
+        returns (bytes4)
+    {
+        (string memory name,
+         uint startPrice,
+         uint64 endTime,
+         uint64 extensionDuration,
+         uint minBidIncrease) = abi.decode(data, (string, uint, uint64, uint64, uint));
+        address payable originalOwner = address(uint160(from));  // address to address payable in Solidity 0.5.x
+        createTLDOffering(name, startPrice, endTime, extensionDuration, minBidIncrease, originalOwner);
         return this.onERC721Received.selector;
     }
 }
