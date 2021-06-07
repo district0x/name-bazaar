d975f5faa429fd596e1333cd6deb180dc87e41cc
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-06-07 13:01:03 +0200

    Hide facebook share button

--- src/name_bazaar/ui/components/share_buttons.cljs
+++ src/name_bazaar/ui/components/share_buttons.cljs
@@ -5,11 +5,6 @@
 (defn share-buttons [{:keys [url title]}]
   [:div.share-buttons
    [:div.title "Share On:"]
-   [:a {:on-click (fn []
-                    (js-invoke js/FB "ui" (clj->js {:method "feed"
-                                                    :link url
-                                                    :caption title})))}
-    [:i.icon.fb]]
    [:a {:target "_blank"
         :href (str "https://twitter.com/intent/tweet?text=" title "&url=" url)}
     [:i.icon.twitter]]])
