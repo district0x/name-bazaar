a627a93ec3c27f8871294d308ba5836a8c205504
Krzysztof Władyka <krzysztof@wladyka.eu>
2022-10-03 22:00:31 +0200

    once more

--- src/name_bazaar/shared/smart_contracts.cljs
+++ src/name_bazaar/shared/smart_contracts.cljs
@@ -1,13 +1,13 @@
 (ns name-bazaar.shared.smart-contracts)
     (def smart-contracts
-      {:ens {:name "ENSRegistry" :address "0x7CF6296490e6048409c20d65f1a453A2EecfBcC0"}
-       :eth-registrar {:name "NameBazaarDevRegistrar" :address "0x63eAf267548000B63667590f91145A316D4B0c3B"}
-       :public-resolver {:name "NamebazaarDevPublicResolver" :address "0x73CbcaE849ddBb4ada0e7Ac2dBefb8ff7eCfb667"}
-       :reverse-registrar {:name "NamebazaarDevReverseRegistrar" :address "0x9E55eBF45565f42265e3FDE7dad0deBBa79F0cF6"}
-       :offering-registry {:name "OfferingRegistry" :address "0x9f015952A762F5f9Ca74AB7dEe169Ceb3A6712A4"}
-       :buy-now-offering {:name "BuyNowOffering" :address "0x483aEA9Ea4964eDC75d7B37fb8B6006e81bdF107"}
-       :buy-now-offering-factory {:name "BuyNowOfferingFactory" :address "0x6891a6fafcd7d18A749041aDEA01874E693c74bE"}
-       :auction-offering {:name "AuctionOffering" :address "0x08A29a709C302885DD937eFA68A6FAc8De5383E7"}
-       :auction-offering-factory {:name "AuctionOfferingFactory" :address "0xaF9a0F973b42e1a706127c3dA917346FbeFe3671"}
-       :district0x-emails {:name "District0xEmails" :address "0x1f0b072e9AC1584Cb000Ae1EdB199220dfF1c460"}
-       :reverse-name-resolver {:name "NamebazaarDevNameResolver" :address "0xD0287e0a9d8AEe9B4c2ACB41cEfF85fcb5306a7D"}})
+      {:ens {:name "ENSRegistry" :address "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"}
+       :eth-registrar {:name "NameBazaarDevRegistrar" :address "0x2375bbB61957806adb48d51b9F1a280d95D11144"}
+       :public-resolver {:name "NamebazaarDevPublicResolver" :address "0xAE65329828CDDc44a753f6A8Dce6E32E1524b886"}
+       :reverse-registrar {:name "NamebazaarDevReverseRegistrar" :address "0xe592779bEA0553B0Ae48036F86eBfe7e054B5e4d"}
+       :offering-registry {:name "OfferingRegistry" :address "0xB0F0A12E08670b44dB5127894aa4e7bf6F597132"}
+       :buy-now-offering {:name "BuyNowOffering" :address "0x7597637C2902c278ea5166e44D3fBa9B0bFFb577"}
+       :buy-now-offering-factory {:name "BuyNowOfferingFactory" :address "0x5d498e434cCb6d7959812e797cF605b6A946c67c"}
+       :auction-offering {:name "AuctionOffering" :address "0x94275f01c2b6722175C7da9714b4d9e6547BA830"}
+       :auction-offering-factory {:name "AuctionOfferingFactory" :address "0x7573122Fd401Ee4348407BbCC34ebD080cf8a2C7"}
+       :district0x-emails {:name "District0xEmails" :address "0x2881C6ca42B25845382CB90D7c1672cd5DA4bdb4"}
+       :reverse-name-resolver {:name "NamebazaarDevNameResolver" :address "0x8258FdA61b2a0F56C7518959ba3c78AfF4b16f7f"}})
