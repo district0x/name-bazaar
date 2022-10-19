f33bdd757fd11ba7641a9146afe783a0ddfe1573
Krzysztof Władyka <krzysztof@wladyka.eu>
2022-10-19 22:14:38 +0200

    district0x/district-server-web3

--- project.clj
+++ project.clj
@@ -43,7 +43,7 @@
                  [district0x/district-server-endpoints "1.0.1"]
                  [district0x/district-server-logging "1.0.6"]
                  [district0x/district-server-smart-contracts "1.4.0-SNAPSHOT"]
-                 [district0x/district-server-web3 "1.2.6"]
+                 [district0x/district-server-web3 "1.3.0-SNAPSHOT"]
                  [district0x/district-server-web3-events "1.1.10"]
                  [district0x/district-ui-logging "1.1.0"]
                  [district0x/district-ui-mobile "1.0.0"]
