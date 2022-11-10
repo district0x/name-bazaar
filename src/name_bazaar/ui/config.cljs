9fc7e5437a90a9e610bb0afc34df5c96b071a86a
Krzysztof Władyka <krzysztof@wladyka.eu>
2022-11-10 13:20:45 +0100

    ropsten->goerli

--- src/name_bazaar/ui/config.cljs
+++ src/name_bazaar/ui/config.cljs
@@ -23,7 +23,7 @@
              :console? true}
    :pushroute-hosts "namebazaar.qa.district0x.io"
    :infura-id "fd0074468fd64e36b495c846a26a3f9d" ;; this is for wallet-connect only, which will not work when running with ganache
-   :fallback-web3-url "https://ropsten.infura.io/"
+   :fallback-web3-url "https://goerli.infura.io/"
    :load-node-addresses? true
    :root-url "https://namebazaar.qa.district0x.io"
    :server-url "https://api.namebazaar.qa.district0x.io"})
