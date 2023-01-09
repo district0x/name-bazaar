48a8548f626d139046ce94859528f132ff21d82e
Krzysztof Władyka <krzysztof@wladyka.eu>
2023-01-09 16:33:46 +0100

    back contract addresses to production

--- src/name_bazaar/shared/smart_contracts.cljs
+++ src/name_bazaar/shared/smart_contracts.cljs
@@ -1,14 +1,13 @@
 (ns name-bazaar.shared.smart-contracts)
-
 (def smart-contracts
   {:ens {:name "ENSRegistry" :address "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"}
    :eth-registrar {:name "NameBazaarDevRegistrar" :address "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"}
-   :offering-registry {:name "OfferingRegistry" :address "0x3Ba421F5Dc39c4451B250590153B7c02C5fE3898"}
-   :buy-now-offering {:name "BuyNowOffering" :address "0xFbcB7dba96375383AAAa1Dd2C85B79Bf2f61a6AC"}
-   :buy-now-offering-factory {:name "BuyNowOfferingFactory" :address "0xAe22111f72a884b3942CC2755b9E43AD90C97785"}
-   :auction-offering {:name "AuctionOffering" :address "0x2D4CBa7458dBF71cdd91a872af7c929E0A49E783"}
-   :auction-offering-factory {:name "AuctionOfferingFactory" :address "0xe22A8f173C27a21Ae73f8Fb68565A70Aae963336"}
-   :district0x-emails {:name "District0xEmails" :address "0x6142077FC24BC86ef7F8624CFE8D9235eBFc5b2F"}
-   :reverse-name-resolver {:name "NamebazaarDevNameResolver" :address "0x0000000000000000000000000000000000000000"}
-   :public-resolver {:name "NamebazaarDevPublicResolver" :address "0xE264d5bb84bA3b8061ADC38D3D76e6674aB91852"}
-   :reverse-registrar {:name "NamebazaarDevReverseRegistrar" :address "0xD5610A08E370051a01fdfe4bB3ddf5270af1aA48"}})
+   :offering-registry {:name "OfferingRegistry" :address "0x5B5B2B386a84546Fa75E9472e3096C26576B6d24"}
+   :buy-now-offering {:name "BuyNowOffering" :address "0x45141Ef2E09412eAd94793617507A0F487249fB0"}
+   :buy-now-offering-factory {:name "BuyNowOfferingFactory" :address "0xF9Bc1b1bBC9C91864e68D861038282ebdcf2449A"}
+   :auction-offering {:name "AuctionOffering" :address "0x9cf308c1d04AeD7eEA994f029cA2E39517C6372f"}
+   :auction-offering-factory {:name "AuctionOfferingFactory" :address "0xF787200012f8d3EA6Ee686577aA73Ed4a3e3ee0C"}
+   :district0x-emails {:name "District0xEmails" :address "0x02441d5dd828CccB3F81ae702eeEccc2142a192e"}
+   :reverse-name-resolver {:name "NamebazaarDevNameResolver" :address "0xA2C122BE93b0074270ebeE7f6b7292C7deB45047"}
+   :public-resolver {:name "NamebazaarDevPublicResolver" :address "0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41"}
+   :reverse-registrar {:name "NamebazaarDevReverseRegistrar" :address "0x084b1c3C81545d370f3634392De611CaaBFf8148"}})
